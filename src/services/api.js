bff4503ef66d0e33791c2f9ed842e9e22d19c384
abhishek-sanda <abhisheksanda@gmail.com>
2025-12-25 08:25:06 +0530

    Change API URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://scs-consultancy-backend.vercel.app/api';
 
 // Contact API
 export const contactAPI = {
